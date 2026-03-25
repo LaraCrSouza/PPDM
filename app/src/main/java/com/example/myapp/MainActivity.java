0b0336cecdc9ecd0fbc1e5d908be21971c0dbc05
LaraCrSouza <souzalaracristina595@gmail.com>
2026-03-25 12:10:13 -0300

    slide show com interface

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,52 +1,42 @@
 package com.example.myapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bntAvancar, bntVoltar;
+    ImageView imageView;
+    Integer imagens[] = new Integer[]{
+            R.drawable.cachorro,
+            R.drawable.gardem,
+            R.drawable.patinho,
+            R.drawable.happy,
+            R.drawable.porquinho
+    };
 
-    Button b ;
-    EditText edMin, edMax;
+    int posicao=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.tv);
-        b = findViewById(R.id.button);
-        edMin=findViewById(R.id.edMin);
-        edMax=findViewById(R.id.edMax);
+        bntVoltar=findViewById(R.id.buttonVoltar);
+        bntAvancar=findViewById(R.id.buttonAvancar);
+        imageView=findViewById(R.id.imageView);
 
-        b.setOnClickListener(v -> {
-            String minStr = edMin.getText().toString();
-            String maxStr = edMax.getText().toString();
-            if(minStr.isEmpty()){
-                edMin.setError("Informe o valor minimo");
-                return;
-            }
-            if (maxStr.isEmpty()){
-                edMax.setError("Informe o valor máximo");
-                return;
-            }
-
-            int min= Integer.parseInt(minStr);
-            int max= Integer.parseInt(maxStr);
-
-            Random random = new Random();
-            int r = random.nextInt(min,max);
-            tv.setText(Integer.toString(r));
+        bntAvancar.setOnClickListener(v -> {
+            imageView.setImageResource(imagens[posicao]);
         });
     }
 }
